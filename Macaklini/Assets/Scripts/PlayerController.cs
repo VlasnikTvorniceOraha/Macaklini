4c97be25919166e1a231e92a91c17882c990420a
VlasnikTvorniceOraha <jaksa.fajs@gmail.com>
2024-11-12 20:03:42 +0100

    Lobby radi

--- Macaklini/Assets/Scripts/PlayerController.cs
+++ Macaklini/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
-    // Start is called before the first frame update
+    NetworkManager networkManager;
+    UnityTransport unityTransport;
+    UIManager uiManager;
+    Rigidbody2D rb2d;
+
+    public float MovementSpeed = 1f;
+
+    public float JumpForce = 1f;
+
+    float horizontal;
     void Start()
     {
-        
+        networkManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+        unityTransport = networkManager.gameObject.GetComponent<UnityTransport>();
+        uiManager = GameObject.Find("UI").GetComponent<UIManager>();
+        rb2d = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (uiManager.gameStarted.Value && IsOwner)
+        {
+            //igra je pocela, omoguci kretanje
+            Movement();
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        if (IsOwner)
+        {
+            rb2d.velocity = new Vector2(horizontal * MovementSpeed, rb2d.velocity.y);
+        }
         
+    }
+
+    void Movement()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
     }
 }
--- Macaklini/Assets/Scripts/UIManager.cs
+++ Macaklini/Assets/Scripts/UIManager.cs
@@ -22,7 +22,11 @@ public class UIManager : NetworkBehaviour
     public GameObject lobbyScreen;
 
     public GameObject PlayerPanel;
-    NetworkVariable<int> playersConnected = new NetworkVariable<int>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    public GameObject background;
+    private NetworkVariable<int> playersConnected = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    public NetworkVariable<bool> gameStarted = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    
 
     
 
@@ -31,6 +35,9 @@ public class UIManager : NetworkBehaviour
         networkManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
         unityTransport = networkManager.gameObject.GetComponent<UnityTransport>();
         networkManager.OnConnectionEvent += PlayerConnected;
+        
+        //playersConnected.OnValueChanged = updateReadyNumber;
+
         playersConnected.OnValueChanged += updateReadyNumber;
         
 
@@ -45,6 +52,7 @@ public class UIManager : NetworkBehaviour
         {
             //Log
             Debug.Log(playersConnected.Value);
+            Debug.Log(gameStarted.Value);
         }
     }
 
@@ -52,56 +60,137 @@ public class UIManager : NetworkBehaviour
 
     private void PlayerConnected(NetworkManager manager, ConnectionEventData data)
     {
-        
-        //ako ima vise od 4, disconnectaj
-        if (playersConnected.Value >= 4)
-        {
-            Debug.Log("Previse igraca");
-            networkManager.Shutdown();
-        }
+        Debug.Log(data.EventType);
 
-        //igrac se joinao, instanciraj objekt u lobiju i igraca na mapi
-
-        HostJoin.SetActive(false);
-        lobbyScreen.SetActive(true);
-        if (IsOwner)
+        if (data.EventType == ConnectionEvent.ClientConnected)
         {
-            playersConnected.Value += 1;
-        }
-        Debug.Log("Player connected: " + data.ClientId);
-        
-        Transform playerPanelList = lobbyScreen.transform.Find("Panel").Find("Players");
-        foreach (Transform playerPanel in playerPanelList)
-        {
-            if (!playerPanel.gameObject.activeSelf)
+            //ako ima vise od 4, disconnectaj
+            if (playersConnected.Value >= 4 || gameStarted.Value)
             {
-                //ukljuci i ispuni tekstom i imenom i slikom
-                playerPanel.gameObject.SetActive(true);
-                playerPanel.Find("Status").GetComponent<TMP_Text>().text = "Connected";
-                playerPanel.Find("Ime").GetComponent<TMP_Text>().text = "Gunster" + data.ClientId;
-                //nesto sa slikom ali to kasnije
-                break;
+                Debug.Log("Previse igraca ili kasnis");
+                networkManager.Shutdown();
+                return;
             }
-        }
 
+            //klijent se spojio, syncaj ga
+            HostJoin.SetActive(false);
+            lobbyScreen.SetActive(true);
+            if (IsServer)
+            {
+                playersConnected.Value += 1;
+            }
+
+            Debug.Log("Player connected: " + data.ClientId);
         
-        
+            Transform playerPanelList = lobbyScreen.transform.Find("Panel").Find("Players");
+            bool[] ukljuceniPaneli = new bool[4] {false, false, false, false};
+            int brojac = 0;
+            foreach (Transform playerPanel in playerPanelList)
+            {
+                if (!playerPanel.gameObject.activeSelf)
+                {
+                    //ukljuci i ispuni tekstom i imenom i slikom
+                    playerPanel.gameObject.SetActive(true);
+                    playerPanel.Find("Status").GetComponent<TMP_Text>().text = "Connected";
+                    //nesto sa slikom ali to kasnije
+                    ukljuceniPaneli[brojac] = true;
+                    break;
+                }
+                else
+                {
+                    ukljuceniPaneli[brojac] = true;
+                }
+                brojac += 1;
+            }
+            //syncaj panele na klijentskoj strani
+            SyncPanelsRpc(ukljuceniPaneli);
+
+
+        }
+        else if (data.EventType == ConnectionEvent.ClientDisconnected)
+        {
+            if (playersConnected.Value >= 4 || gameStarted.Value)
+            {
+                //netko zajebava
+                return;
+            }
+
+            //klijent se disconnectao, smanji broj spojenih i tako
+            if (IsServer)
+            {
+                playersConnected.Value -= 1;
+            }
+            //trebalo bi spremiti ideve i to ali neda mi se tako da samo ugasi zadnji ukljuceni panel
+            Transform playerPanelList = lobbyScreen.transform.Find("Panel").Find("Players");
+            bool[] ukljuceniPaneli = new bool[4] {false, false, false, false};
+            int brojac = 0;
+            foreach (Transform playerPanel in playerPanelList)
+            {
+                if (!playerPanel.gameObject.activeSelf)
+                {
+                    //ugasi prethodni
+                    playerPanelList.GetChild(brojac - 1).gameObject.SetActive(false);
+                    ukljuceniPaneli[brojac - 1] = false;
+                    break;
+                }
+                else if (playerPanel.gameObject.activeSelf && brojac == 3)
+                {
+                    //svi su upaljeni, ugasi ovaj
+                    playerPanel.gameObject.SetActive(false);
+                    ukljuceniPaneli[brojac] = false;
+                }
+                else
+                {
+                   ukljuceniPaneli[brojac] = true; 
+                }
+                brojac += 1;
+            }
+
+            SyncPanelsRpc(ukljuceniPaneli);
+
+        }
 
 
 
     }
 
     [Rpc(SendTo.NotServer)]
-    private void SyncPanelsRpc()
+    private void SyncPanelsRpc(bool[] ukljuceniPaneli)
     {
         //RPC za syncanje panela na svim klijentima
+        Transform lobbyPlayers = lobbyScreen.transform.Find("Panel").Find("Players");
+
+        for (int i = 0; i < ukljuceniPaneli.Length; i++)
+        {
+            //panel je ukljucen
+            if (ukljuceniPaneli[i])
+            {
+                lobbyPlayers.GetChild(i).gameObject.SetActive(true);
+                lobbyPlayers.GetChild(i).Find("Status").GetComponent<TMP_Text>().text = "Connected";
+            }
+            else
+            {
+                //panel nije ukljucen
+                lobbyPlayers.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+
+        //imena kasnije isto ig
         
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void StartGameRpc()
+    {
+        //zapocni igru i osposobi kretanje svim igracima
+        lobbyScreen.SetActive(false);
+        background.SetActive(false);
     }
     
 
     public void updateReadyNumber(int prevValue, int newValue)
     {
-        Debug.Log("Number of players: " + playersConnected.Value);
+        Debug.Log("Number of players: " + newValue);
         //updateaj ready text
         lobbyScreen.transform.Find("Panel").Find("Ready").GetComponent<TMP_Text>().text = newValue + "/4";
     }
@@ -155,7 +244,30 @@ public class UIManager : NetworkBehaviour
         
     }
 
+    public void StartGameButton()
+    {
+        if (!IsServer)
+        {
+            Debug.Log("Nemoj zajebavati");
+            return;
+        }
+
+        gameStarted.Value = true;
+
+        StartGameRpc();
+
+    }
+
     
+
+    public void ExitLobby()
+    {
+        
+        networkManager.Shutdown();
+        HostJoin.SetActive(true);
+        lobbyScreen.SetActive(false);
+
+    }
     
     public void ExitButton()
     {
